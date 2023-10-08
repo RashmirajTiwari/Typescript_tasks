a82418b1232fb84dd17d37cd34b7961170bb9dd6
Rashmiraj Tiwari <rashmirajt@athmin.com>
2023-10-08 20:33:09 +0530

    Worked on Array,objects,generic Array,generic promise of Typescript

--- app.ts
+++ app.ts
@@ -1,1 +1,48 @@
-console.log("Just Starting").
+
+const num1Element=document.getElementById("num1") as HTMLInputElement;
+const num2Element=document.getElementById("num2") as HTMLInputElement;
+const buttonElement=document.querySelector("button")!;
+
+const numResult:Array<number>=[]; //generic type
+const textResult:string[]=[];
+
+type NumOrString=number|string;
+type result={val:number;timestamp:Date}
+
+function add(num1:NumOrString,num2:NumOrString){
+    if(typeof num1==='number' && typeof num2==='number'){
+        return num1+num2;
+    }else if(typeof num1==='string' && typeof num2==='string'){
+        return num1+" "+num2;
+    }
+    return +num1 + +num2;
+}
+ function printResult(resultObj:result){
+    console.log(resultObj.val);
+ }
+
+
+
+
+buttonElement.addEventListener("click",()=>{
+    const num1=num1Element.value;
+    const num2=num2Element.value;
+    const result=add(+num1,+num2);
+    const stringResult=add(num1,num2);
+    numResult.push(result as number);
+    textResult.push(stringResult as string);
+
+    printResult({val:result as number,timestamp:new Date()})
+    console.log(numResult,textResult)
+
+
+})
+
+const myPromise=new Promise<string>((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('It Worked');
+    },1000)
+})
+myPromise.then((result)=>{
+    console.log(result.split('W'))
+})
